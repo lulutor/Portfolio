598908ef9e76e6c646a62a98f1af005d1187b84f
lucas <lucas.nandan@gmail.com>
2018-07-20 16:45:52 +0200

    ajout jquerry creations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,78 @@
 // Parallax jQuery
 $(document).ready(function(){
-    $('.parallax').parallax();
+  $('.parallax').parallax();
+ 
+
+  //Navbar jQuery
+
+    $('.sidenav').sidenav();
+
+
+  //Animation : click compétence ==> Apparition Création
+
+ var dataDom = []
+
+  var $body = $('body');
+  var $html = $('#html');
+  var $bootstrap = $('#bootstrap');
+  var $javascript = $('#javascript');
+  var $prestashop = $('#prestashop');
+  var $angular = $('#angular');
+  var $react = $('#react');
+
+  var $htmlCreation = $('#html-creation');
+  var $bootstrapCreation = $('#bootstrap-creation');
+  var $javascriptCreation = $('#javascript-creation');
+  var $javascript2Creation = $('#javascript2-creation');
+  var $prestashopCreation = $('#prestashop-creation');
+  var $angularCreation= $('#angular-creation');
+  var $reactCreation = $('#react-creation');
+
+  function hide() {  
+    $htmlCreation.hide();
+    $bootstrapCreation.hide();
+    $javascriptCreation.hide();
+    $javascript2Creation.hide();
+    $prestashopCreation.hide();
+    $angularCreation.hide();
+    $reactCreation.hide();
+  }
+
+  hide();
+  
+  $body.fadeIn(1000);
+
+  $html.on('click',function(){
+    hide();
+    $htmlCreation.fadeIn(800);
+    
   });
+
+  $bootstrap.on('click',function(){
+    hide();
+    $bootstrapCreation.fadeIn(800);
+  });
+
+  $javascript.on('click',function(){
+    hide();
+    $javascriptCreation.fadeIn(800);
+    $javascript2Creation.fadeIn(800);
+  });
+
+  $prestashop.on('click',function(){
+    hide();
+    $prestashopCreation.fadeIn(800);
+  });
+
+  $angular.on('click',function(){
+    hide();
+    $angularCreation.fadeIn(800);
+  });
+
+  $react.on('click',function(){
+    hide();
+    $reactCreation.fadeIn(800);
+  }); 
+
+
+});
